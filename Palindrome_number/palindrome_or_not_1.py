eac83370ecbcb626c8363e9d7c44c9bd32965b15
DeepakAdoor05 <deepakadoor726@gmail.com>
2025-02-07 23:05:49 +0530

    Check palindrome taking input as string

---
+++ Palindrome_number/palindrome_or_not_1.py
@@ -0,0 +1,8 @@
+def palindrome(no):
+    if no == no[ : :-1] :
+        print("Palindrome")
+    else:
+        print("Not Palindrome")
+
+number = input() # Taking the number as string.
+palindrome(number)
