5e39af17c5426194b5ec8198577aaabc66081a93
DeepakAdoor05 <deepakadoor726@gmail.com>
2025-02-11 22:00:29 +0530

    Find term of the given prime_number

---
+++ Prime_no/count_prime/count_prime.py
@@ -0,0 +1,19 @@
+def is_prime(num):
+    flag = True
+    if num > 1:
+        for i in range(2,num):
+            if (num%i) == 0:
+                flag = False
+                break
+        return flag
+    return False
+n = 5   # Prime number
+prime_count = 0
+if False  is is_prime(n):
+    print("Not a Prime no")
+else:
+    while n > 1 :
+        if is_prime(n):
+            prime_count += 1
+        n -= 1
+    print(prime_count)  # which term is the given prime number. 3rd term is 5 (2,5,7...)
