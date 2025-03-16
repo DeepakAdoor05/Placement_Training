c18215996d8b9fa072262efa8ee614983d09b747
DeepakAdoor05 <deepakadoor726@gmail.com>
2025-03-16 21:13:12 +0530

    Sum of fibonacci series is commented

--- Fibonacci/Fibonacci_series/fibonacci_recursion.py
+++ Fibonacci/Fibonacci_series/fibonacci_recursion.py
@@ -5,6 +5,9 @@ def fib_rec(n):
         return fib_rec(n-1) + fib_rec(n-2)
     
 number = 5
+sum = 0
 for i in range(number):
-    print(fib_rec(i))
+    print(fib_rec(i),end = " ")              # (Display the fibonacci series,  0 1 1 2 3)
+#     sum += fib_rec(i)
+# print("\n",sum)          # 0+1+1+2+3 = 7 (Display the Sum of fibonacci series)
 # print(fib_rec(3))     This will give the nth term (starts with 0) 3rd term will be '2'.
