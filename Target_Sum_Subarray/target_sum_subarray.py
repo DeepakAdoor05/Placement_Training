dac9cfef884168354d186dd66f5bc05f51e48e6b
DeepakAdoor05 <deepakadoor726@gmail.com>
2025-03-16 11:50:20 +0530

    Target Sum Subarray problem

---
+++ Target_Sum_Subarray/target_sum_subarray.py
@@ -0,0 +1,16 @@
+def find_subarray(arr,target):
+    result = []
+    for i in range(len(arr)):
+        sum = 0
+        subarray = []
+        for j in range(i,len(arr)):
+            sum += arr[j]
+            subarray.append(arr[j])
+            if sum == target:
+                result.append(subarray[:])
+    print(result)
+
+
+arr = [1,2,3,4,5,67]
+target = 5
+find_subarray(arr,target)
